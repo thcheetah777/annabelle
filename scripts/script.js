fa432c21fa8cb00cc9fe377fb97c866acb3450e1
thcheetah777 <thcheetah777@gmail.com>
2022-07-25 22:06:01 +0800

    Finished

--- scripts/script.js
+++ scripts/script.js
@@ -9,35 +9,11 @@ let circle8 = document.getElementById("circle8");
 let circle9 = document.getElementById("circle9");
 let circlesArray = [circle1, circle2, circle3, circle4, circle5, circle6, circle7, circle8, circle9];
 
-let defaultTurnSpeed = 0.5;
-
-for (let x = 0; x < circlesArray.length; x++) {
-  const element = circlesArray[x];
-  element.turnSpeed = defaultTurnSpeed;
-  element.degrees = 0;
-}
-
-setInterval(() => {
-  for (let x = 0; x < circlesArray.length; x++) {
-    const element = circlesArray[x];
-    if (x % 2 === 0) {
-      element.style.transform = `rotate(-${element.degrees}deg)`;
-    } else {
-      element.style.transform = `rotate(${element.degrees}deg)`;
-    }
-    element.degrees += element.turnSpeed;
-  }
-}, 1);
-
-for (let x = 0; x < circlesArray.length; x++) {
-  const element = circlesArray[x];
-}
 circlesArray.forEach(element => {
   element.onmouseover = () => {
-    console.log("l");
-    element.turnSpeed = 0.1;
+    element.style
   }
   element.onmouseout = () => {
-    element.turnSpeed = defaultTurnSpeed;
+    element.style
   }  
 });
